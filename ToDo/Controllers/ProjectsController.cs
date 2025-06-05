f6bc6a07eac342602a9804224ae796b275a67fe0
AlexanederNedelkin <avnedelkin@gmail.com>
2025-06-05 22:01:03 +0300

    Added ProjectController

---
+++ ToDo/Controllers/ProjectsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using ToDo.Core.Contracts;
+using ToDo.Core.Models.ViewModels;
+
+namespace ToDo.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        private readonly IProjectService _projectService;
+        public ProjectsController(IProjectService projectService)
+        {
+            _projectService = projectService;
+        }
+
+        [Authorize]
+        [HttpGet("/")]
+        public async Task<IActionResult> Index()
+        {
+            return View(new ProjectsVM(User.Identity.Name, await _projectService.GetUserProjects(User.FindFirstValue(ClaimTypes.NameIdentifier))));
+        }
+    }
+}

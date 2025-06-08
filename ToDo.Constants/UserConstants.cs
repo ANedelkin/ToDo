d12e82476d4f7a4b05b0dcf889f1390359c6b275
AlexanederNedelkin <avnedelkin@gmail.com>
2025-06-08 16:43:51 +0300

    Stylized login and register pages

---
+++ ToDo.Constants/UserConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo.Constants
+{
+    public static class UserConstants
+    {
+        public const string userNameRegex = "^[a-zA-Z0-9][a-zA-Z0-9-_]{2,18}[a-zA-Z0-9]$";
+    }
+}
